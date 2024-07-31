eb72344d2ff0c2ef77664107269b0147b91bdf37
DJAVELIN <mrdjavelin@gmail.com>
2024-07-31 22:30:32 +0300

    info page

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -7,84 +7,116 @@ const InfoPage = () => {
                     fontFamily: "Consolas Menlo Monaco Courier New, monospace",
                 }}
             >
-                https://js-clock-wallpaper.vercel.app/<b>?lang=javascript</b>&
-                <b>style=monokaiSublime</b>
+                <a href="https://js-clock-wallpaper.vercel.app/?lang=javascript&style=monokaiSublime">
+                    https://js-clock-wallpaper.vercel.app/
+                    <b>?lang=javascript</b>&<b>style=monokaiSublime</b>
+                </a>
             </h3>
             <div className="infoblock">
-                <label>List of supported languages</label>
-                <ul>
+                <label>List of supported languages:</label>
+                <ul id="lang">
                     <li>
-                        <i>(default)</i> Javascript{" "}
-                        <a href="/?lang=javascript" target="_blank">
+                        Javascript{" "}
+                        <a
+                            href="/?lang=javascript&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         Python{" "}
-                        <a href="/?lang=python" target="_blank">
+                        <a
+                            href="/?lang=python&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         CSharp{" "}
-                        <a href="/?lang=csharp" target="_blank">
+                        <a
+                            href="/?lang=csharp&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         C++{" "}
-                        <a href="/?lang=cpp" target="_blank">
+                        <a
+                            href="/?lang=cpp&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         Rust{" "}
-                        <a href="/?lang=rust" target="_blank">
+                        <a
+                            href="/?lang=rust&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         TypeScript{" "}
-                        <a href="/?lang=typescript" target="_blank">
+                        <a
+                            href="/?lang=typescript&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         Java{" "}
-                        <a href="/?lang=java" target="_blank">
+                        <a
+                            href="/?lang=java&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                 </ul>
-                <label>List of supported themes/styles</label>
-                <ul>
+                <label>List of supported themes/styles:</label>
+                <ul id="themes">
                     <li>
-                        <i>(default)</i> monokaiSublime{" "}
-                        <a href="/?style=monokaiSublime" target="_blank">
+                        monokaiSublime{" "}
+                        <a
+                            href="/?lang=javascript&style=monokaiSublime"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         monokai{" "}
-                        <a href="/?style=monokai" target="_blank">
+                        <a
+                            href="/?lang=javascript&style=monokai"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         dracula{" "}
-                        <a href="/?style=dracula" target="_blank">
+                        <a
+                            href="/?lang=javascript&style=dracula"
+                            target="_blank"
+                        >
                             click to view
                         </a>
                     </li>
                     <li>
                         dark{" "}
-                        <a href="/?style=dark" target="_blank">
+                        <a href="/?lang=javascript&style=dark" target="_blank">
                             click to view
                         </a>
                     </li>
                     <li>
                         light{" "}
-                        <a href="/?style=light" target="_blank">
+                        <a href="/?lang=javascript&style=light" target="_blank">
                             click to view
                         </a>
                     </li>
